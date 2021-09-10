f732f4b3c73ab35e159b68422e6461888d66be1a
antonio rivas nieto <rivas020600@gmail.com>
2021-09-10 18:48:19 -0500

    Revert ":sparkles: aporte"
    This reverts commit f965b339f3897f560be4a44166928370b4a20db0.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express()
 app.get('/posts', (req, res, next) => {
     console.log("I get posts")
     console.log("Soy un aporte")
-    console.log("Soy yo un segundo aporte")
 })
 
 app.listen(3000) 
